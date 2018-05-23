f90505776666dce157c7f9efeb9f39471b726a42
Mike <mike@tofet.net>
2018-05-22 21:52:52 -0700

    task list
    Working on task list generation

--- MVP/js/listGen.js
+++ MVP/js/listGen.js
@@ -16,13 +16,29 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-function createArrayOfWho() {
-	// Creates an array of all the "Who"
+function createArrayOfTasks() {
+	// Creates an array of all the "Who", with their list of tasks
 	// Each entry is an object:
 	// {who: "theirName"
 	// Thoughts
 	/*
 		- Go through each table row, find an active project
 				- For this project, go through task list
-					- For each task list entry 
-	}
+					- For each task list entry create an entry in a dictionary:
+						{ [who]:[{ProjectName: " ", what: " ", ProjectPri: " ", when:" "}, {repeat}], [nextWho]}
+		- Issues
+			- "Who" might be multiple words, so needs to be concatenated by a "_" (or other valid space designator)
+			- "Who" matching should be case insensitive
+			- If a "Who" isn't found, then it needs to be added to the object
+			- Projects may have more than one task assigned to each Who, so a bit of redundant data here
+			
+		
+	*/
+
+// If we select on "priority-row-" we will get all of the items in the priority row
+// Then we have to get the id of the object, and use this in the "priorityChips" object to get the projectID
+// Then we get the list of tasks from projects[projectID]["tasks"] and each has a "who", "what" , "when";
+//  Project Pri has to be pulled from the "-slot-x" addendum to each "priority-row-" and calculated to match the categories in priorityRowData
+// Project name is projects[id]["title"]
+//  				
+};
