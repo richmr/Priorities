17a172b4b3a05423596addd2ef58f5e5959e9d6a
bernie <bernie@bernie-box-2020.1>
2020-02-17 20:56:31 -0800

    Added dbauth template.

---
+++ MVP/util/dbauth_TEMPLATE.php
@@ -0,0 +1,8 @@
+<?php
+  // This should be filled out with the actual credentials and renamed "dbauth.php"
+  // Ensure your .gitignore is ignoring dbauth.php BEFORE committing to GIT
+  $db_server = "";
+  $db_name = "";
+  $db_user = "";
+  $db_pass = "";
+ ?>
